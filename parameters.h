c25d390bd6910fe96bcff4601f05c9ddff16a045
akh <burzhymskyi.rostyslav@gmail.com>
2024-12-15 21:57:55 +0200

    seems like problem already fixed with bites from prev commit but need tests with Frodo.Decode

--- parameters.h
+++ parameters.h
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #ifndef parameters
 #define parameters
 
@@ -8,5 +10,5 @@
 #define B 2
 #define nm 8
 #define MATRIX_LEN 64
-
+#define B_IN_16 int(ceil((double)B / 16.0))
 #endif
