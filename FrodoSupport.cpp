86d669b2c272a33319fca4fd95487f4f40e906da
akh <burzhymskyi.rostyslav@gmail.com>
2024-12-29 20:01:25 +0200

    frodo encode and decode tested

--- FrodoSupport.cpp
+++ FrodoSupport.cpp
@@ -1,4 +1,3 @@
-
 #include "FrodoSupport.h"
 #include "parameters.h"
 
@@ -9,257 +8,283 @@
 #include <iterator>
 #include <string>
 
-void frodo_encode(uint16_t* K, uint16_t* k)
+void frodo_encode(uint16_t *K, uint16_t *k)
 {
-    size_t mask = (1 << B) - 1;
-    size_t mask16bit = (1 << 16) - 1;
-    size_t bitIter = 0;
+	size_t mask = (1 << B) - 1;
+	size_t mask16bit = (1 << 16) - 1;
+	size_t bitIter = 0;
 
-    size_t mul = (1 << (D - B));
-    size_t kIter = 0;
-    uint16_t tmp = 0;
-    int v = B_IN_16 - 1;
-    for (size_t i = 0; i < MATRIX_LEN; ++i)
-    {
-        tmp |= ((k[kIter] >> bitIter) & mask);
-        K[i] = mul * tmp;
-        bitIter = (bitIter + B) % B_IN_16;
-        tmp = 0;
-        #ifndef FRODO
-        if (i % v == 0 && i > 0)
+	size_t mul = (1 << (D - B));
+	size_t kIter = 0;
+	uint16_t tmp = 0;
+	int v = B_IN_16;
+	size_t bit = 0;
+	for (int i = 0; i < MATRIX_LEN; ++i)
+	{
+#ifndef FRODO
+        if ((bitIter == 14 || bitIter == 15) && i > 0 && i % 15 != 0 )
         {
-            tmp = ((k[kIter] >> bitIter) & (mask));
-            ++kIter;
-            bitIter = B - bitIter;
+            tmp = ((k[bit/ 16] >>  bitIter ) & (mask >> (B - 16 + bitIter)));
+            bit +=  16 - bitIter;
+            tmp |= ((k[bit / 16] ) & (mask >> (16 - bitIter))) << (16 - bitIter);
+            bit +=B - 16 + bitIter;
+            K[i] = mul * tmp;
+            bitIter = B -  16 + bitIter ;
+            tmp = 0;
+            continue;
         }
-        #endif
+#else
+		if (i % v == 0 && i > 0)
+		{
+			++kIter;
+		}
+#endif
 
-    }
+		tmp |= ((k[bit / 16] >> bitIter) & mask);
+		K[i] = mul * tmp;
+		bitIter = (bitIter + B) % 16;
+		bit += B;
+		tmp = 0;
+	}
 }
 
-void frodo_decode(uint16_t* K, uint16_t* k)
+void frodo_decode(uint16_t *K, uint16_t *k)
 {
-    size_t modB = (1 << B) - 1; //  mod 2^B
-    size_t mask16bit = (1 << 16) - 1;
-    double mul = double(1 << B) / double(1 << D); // 2^B / q
+	size_t mask = (1 << B) - 1; //  mod 2^B
+	size_t mask16bit = (1 << 16) - 1;
+	double mul = double(1 << B) / double(1 << D); // 2^B / q
 
-    size_t kIter = 0;
-    size_t bitIter = 0;
-    int tmp = 0;
-    for (size_t i = 0; i < MATRIX_LEN; ++i)
-    {
-        bitIter &= mask16bit; // change ^ to & for mod operation
-        tmp = uint16_t((0.5 + K[i] * mul)) & modB;
-        if (i % B_IN_16 == 0 && i > 0)
+	size_t kIter = 0;
+	size_t bitIter = 0;
+	int tmp = 0;
+	int v = B_IN_16;
+	size_t bit = 0;
+	for (size_t i = 0; i < MATRIX_LEN; ++i)
+	{
+#ifndef FRODO
+        if ((bitIter == 15 || bitIter == 14) && i > 0 && i % 15 != 0 )
         {
-            k[kIter] |= (tmp << (bitIter)&mask16bit);
-            ++kIter;
-            k[kIter] = tmp >> (B - 16 + bitIter);
-            bitIter &= mask16bit; // change ^ to & for mod operation
+            tmp = uint16_t(double(K[i]) * mul + 0.5) & mask;
+            k[bit / 16] |= (tmp << bitIter) & mask16bit;
+            // tmp = ((k[kIter] >>  bitIter ) & (mask >> (16 - bitIter)));
+        	bit += 16 - bitIter;
+            k[bit / 16] |= tmp >> ( 16 - bitIter);
+        	bit += B - 16 + bitIter;
+            // tmp |= ((k[kIter] ) & (mask >> (16 - bitIter))) << (16 - bitIter);
+            bitIter = B -  16 + bitIter ;
+
+            continue;
         }
-        else
-        {
-            k[kIter] |= tmp << bitIter;
-        }
-        bitIter += B;
-    }
+#else
+		if (i % v == 0 && i > 0)
+		{
+			++kIter;
+		}
+#endif
+		tmp = uint16_t(double(K[i]) * mul + 0.5) & mask;
+		k[bit / 16] |= tmp << bitIter;
+		bitIter = (bitIter + B) % 16;
+		bit += B;
+	}
 }
 
 std::uint16_t rotate16bit(uint16_t a)
 {
-    uint16_t mask16 = (1 << 16) - 1;
-    uint32_t mask15 = (1 << 15);
-    for (int i = 0; i < 8; ++i)
-    {
-        uint16_t mask = (1 << 16) - 1;
-        mask = (mask) ^ (1 << 15 - i) & (mask) ^ (1 << i);
-        uint16_t tmp =(a << 15 - 2*i);
-        uint16_t bit1 = ((a << 15 - 2*i) & (mask15 >> i));
-        uint16_t bit2 = ((a >> 15 - 2*i) & (1 << i));
-        a = (mask & a) | ((a << 15 - 2*i) & (mask15 >> i)) | ((a >> 15 - 2*i) & (1 << i));
-    }
-    if (D == 15)
-        a = a >> 1;
-    return a;
+	uint16_t mask16 = (1 << 16) - 1;
+	uint32_t mask15 = (1 << 15);
+	for (int i = 0; i < 8; ++i)
+	{
+		uint16_t mask = (1 << 16) - 1;
+		mask = (mask) ^ (1 << 15 - i) & (mask) ^ (1 << i);
+		uint16_t tmp = (a << 15 - 2 * i);
+		uint16_t bit1 = ((a << 15 - 2 * i) & (mask15 >> i));
+		uint16_t bit2 = ((a >> 15 - 2 * i) & (1 << i));
+		a = (mask & a) | ((a << 15 - 2 * i) & (mask15 >> i)) | ((a >> 15 - 2 * i) & (1 << i));
+	}
+	if (D == 15)
+		a = a >> 1;
+	return a;
 }
 
-void frodo_pack(uint16_t* C, uint8_t* b, uint16_t n1, uint16_t n2)
+void frodo_pack(uint16_t *C, uint8_t *b, uint16_t n1, uint16_t n2)
 {
-    size_t matrix_len = n1 * n2;
-    size_t mask = (1 << 8) - 1;
-    size_t kIter = 0;
-    size_t bitIter = 0;
-    int bitCut = 8;
-    size_t bit = 0;
-    uint16_t tmp = 0;
-    int i = 0;
-    int bI = 0;
+	size_t matrix_len = n1 * n2;
+	size_t mask = (1 << 8) - 1;
+	size_t kIter = 0;
+	size_t bitIter = 0;
+	int bitCut = 8;
+	size_t bit = 0;
+	uint16_t tmp = 0;
+	int i = 0;
+	int bI = 0;
 
-    while (i < matrix_len)
-    {
-        tmp = rotate16bit(C[i]);
+	while (i < matrix_len)
+	{
+		tmp = rotate16bit(C[i]);
 
-        // maybe bitIter and bit should make in one
-        // parametr, and take while % D != 0, and if % 8 == 0
-        while (bitIter != D)
-        {
-            b[bit / 8] |= ((((tmp >> bitIter) & (mask >> (8 - bitCut))))) << (bit % 8);
-            bitIter += bitCut;
-            bit += bitCut;
-            if(D - bitIter == 0)
-            {
-                bitCut = 8 - bitCut;
-                break;
-            }
-            if(D - bitIter >= 8)
-            {
-                bitCut = 8;
-                continue;
-            }
-            bitCut = (D - bitIter); // mod
+		// maybe bitIter and bit should make in one
+		// parametr, and take while % D != 0, and if % 8 == 0
+		while (bitIter != D)
+		{
+			b[bit / 8] |= ((((tmp >> bitIter) & (mask >> (8 - bitCut))))) << (bit % 8);
+			bitIter += bitCut;
+			bit += bitCut;
+			if (D - bitIter == 0)
+			{
+				bitCut = 8 - bitCut;
+				break;
+			}
+			if (D - bitIter >= 8)
+			{
+				bitCut = 8;
+				continue;
+			}
+			bitCut = (D - bitIter); // mod
 
-            bI = bit / 8;
-        }
+			bI = bit / 8;
+		}
 
-        bitIter = 0;
-        i += 1;
-    }
+		bitIter = 0;
+		i += 1;
+	}
 }
 
-void unrotate16bit(uint16_t& a)
+void unrotate16bit(uint16_t &a)
 {
-    if (D == 15)
-        a = a << 1;
-    uint16_t mask16 = (1 << 16) - 1;
-    uint32_t mask15 = (1 << 15);
-    for (int i = 0; i < 8; ++i)
-    {
-        uint16_t mask = (1 << 16) - 1;
-        mask = (mask) ^ (1 << 15 - i) & (mask) ^ (1 << i);
-        uint16_t tmp =(a << 15 - 2*i);
-        uint16_t bit1 = ((a << 15 - 2*i) & (mask15 >> i));
-        uint16_t bit2 = ((a >> 15 - 2*i) & (1 << i));
-        a = (mask & a) | ((a << 15 - 2*i) & (mask15 >> i)) | ((a >> 15 - 2*i) & (1 << i));
-    }
+	if (D == 15)
+		a = a << 1;
+	uint16_t mask16 = (1 << 16) - 1;
+	uint32_t mask15 = (1 << 15);
+	for (int i = 0; i < 8; ++i)
+	{
+		uint16_t mask = (1 << 16) - 1;
+		mask = (mask) ^ (1 << 15 - i) & (mask) ^ (1 << i);
+		uint16_t tmp = (a << 15 - 2 * i);
+		uint16_t bit1 = ((a << 15 - 2 * i) & (mask15 >> i));
+		uint16_t bit2 = ((a >> 15 - 2 * i) & (1 << i));
+		a = (mask & a) | ((a << 15 - 2 * i) & (mask15 >> i)) | ((a >> 15 - 2 * i) & (1 << i));
+	}
 }
 
-void frodo_unpack(uint16_t* C, uint8_t* b, uint16_t n1, uint16_t n2)
+void frodo_unpack(uint16_t *C, uint8_t *b, uint16_t n1, uint16_t n2)
 {
-    size_t matrix_len = n1 * n2;
-    size_t mask = (1 << 8) - 1;
-    size_t kIter = 0;
-    int bitIter = 0;
-    int bitCut = 8;
-    size_t bit = 0;
-    size_t bit8Cut = 0;
-    uint8_t tmp = b[0];
-    int i = 0;
-    int bI = 0;
-    int bitCutPrev = 0;
-    while (i < matrix_len)
-    {
+	size_t matrix_len = n1 * n2;
+	size_t mask = (1 << 8) - 1;
+	size_t kIter = 0;
+	int bitIter = 0;
+	int bitCut = 8;
+	size_t bit = 0;
+	size_t bit8Cut = 0;
+	uint8_t tmp = b[0];
+	int i = 0;
+	int bI = 0;
+	int bitCutPrev = 0;
+	while (i < matrix_len)
+	{
+		while (bitIter != D)
+		{
+			C[i] |= (b[bit / 8] >> bit8Cut & (mask >> 8 - bitCut)) << (bitIter);
 
-        while (bitIter != D)
-        {
-            C[i] |= (b[bit / 8] >> bit8Cut  & (mask >> 8 - bitCut)) << (bitIter);
+			bitIter += bitCut;
+			bit += bitCut;
+			bit8Cut = (bit8Cut + bitCut) % 8;
+			if (D - bitIter == 0)
+			{
+				bitCut = 8 - bitCut;
+				break;
+			}
+			if (D - bitIter >= 8)
+			{
+				bitCut = 8;
+				continue;
+			}
+			bitCut = (D - bitIter);
 
-            bitIter += bitCut;
-            bit += bitCut;
-            bit8Cut = (bit8Cut + bitCut) % 8;
-            if(D - bitIter == 0)
-            {
-                bitCut = 8 - bitCut;
-                break;
-            }
-            if(D - bitIter >= 8)
-            {
-                bitCut = 8;
-                continue;
-            }
-            bitCut = (D - bitIter);
+			bI = bit / 8;
+		}
 
-            bI = bit / 8;
+		bitIter = 0;
 
-        }
-
-        bitIter = 0;
-
-        unrotate16bit(C[i]);
-        ++i;
-    }
+		unrotate16bit(C[i]);
+		++i;
+	}
 }
 
-uint32_t hexCharToNumber(char& hex)
+uint32_t hexCharToNumber(char &hex)
 {
-    switch (hex)
-    {
-        case 'A':
-            return 10;
-        case 'B':
-            return 11;
-        case 'C':
-            return 12;
-        case 'D':
-            return 13;
-        case 'E':
-            return 14;
-        case 'F':
-            return 15;
-        default:
-            if (hex > '0' && hex <= '9')
-                return hex - '0';
-            return 0; // TODO here must be exception, when get number32bit from
-                      // other system
-    }
+	switch (hex)
+	{
+		case 'A':
+			return 10;
+		case 'B':
+			return 11;
+		case 'C':
+			return 12;
+		case 'D':
+			return 13;
+		case 'E':
+			return 14;
+		case 'F':
+			return 15;
+		default:
+			if (hex > '0' && hex <= '9')
+				return hex - '0';
+			return 0; // TODO here must be exception, when get number32bit from
+		// other system
+	}
 }
 
 void frodo_uint16(uint16_t *H, size_t len, std::string hex)
 {
-    size_t i_hex = 0;
-    std::reverse(hex.begin(), hex.end());
-    for(int i = 0; i < len; ++i)
-    {
-        for(int j = 0; j < 4; ++j)
-        {
-            H[i] |= hexCharToNumber(hex[i_hex + j]) << j*4;
-        }
-        i_hex += 4;
-    }
+	size_t i_hex = 0;
+	std::reverse(hex.begin(), hex.end());
+	for (int i = 0; i < len; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			H[i] |= hexCharToNumber(hex[i_hex + j]) << j * 4;
+		}
+		i_hex += 4;
+	}
 }
 
 
-std::string frodo_hex(uint16_t* H, size_t len)
+std::string frodo_hex(uint16_t *H, size_t len)
 {
-    std::string result = "";
-    uint16_t mask = (1 << 4) - 1;
-    uint16_t shift = 0;
-    char numHex[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                       '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    for (int i = 0; i < len; ++i)
-    {
-        for (int j = 0; j < 16; j += 4)
-        {
-            uint16_t debug = H[i] >> j;
-            result = numHex[(H[i] >> j) & mask] + result;
-        }
-    }
-    return result;
+	std::string result = "";
+	uint16_t mask = (1 << 4) - 1;
+	uint16_t shift = 0;
+	char numHex[16] = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+	};
+	for (int i = 0; i < len; ++i)
+	{
+		for (int j = 0; j < 16; j += 4)
+		{
+			uint16_t debug = H[i] >> j;
+			result = numHex[(H[i] >> j) & mask] + result;
+		}
+	}
+	return result;
 }
 
-std::string frodo_hex(uint8_t* H, size_t len)
+std::string frodo_hex(uint8_t *H, size_t len)
 {
-    std::string result = "";
-    uint16_t mask = (1 << 4) - 1;
-    uint16_t shift = 0;
-    char numHex[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                       '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    for (int i = 0; i < len; ++i)
-    {
-        for (int j = 0; j < 8; j += 4)
-        {
-            uint16_t debug = H[i] >> j;
-            result = numHex[(H[i] >> j) & mask] + result;
-        }
-    }
-    return result;
+	std::string result = "";
+	uint16_t mask = (1 << 4) - 1;
+	uint16_t shift = 0;
+	char numHex[16] = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+	};
+	for (int i = 0; i < len; ++i)
+	{
+		for (int j = 0; j < 8; j += 4)
+		{
+			uint16_t debug = H[i] >> j;
+			result = numHex[(H[i] >> j) & mask] + result;
+		}
+	}
+	return result;
 }
--- main.cpp
+++ main.cpp
@@ -6,44 +6,61 @@
 
 int generateRandomNumber(int bit)
 {
-    const int min = 0;
-    const int max = (1 << bit); // 2^15
+	const int min = 0;
+	const int max = (1 << bit); // 2^15
 
-    std::random_device rd;  // Seed generator
-    std::mt19937 gen(rd()); // Mersenne Twister generator
-    std::uniform_int_distribution<> dis(min, max);
+	std::random_device rd; // Seed generator
+	std::mt19937 gen(rd()); // Mersenne Twister generator
+	std::uniform_int_distribution<> dis(min, max);
 
-    return dis(gen);
+	return dis(gen);
 }
 
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int matrix_len = 2 * 4;
-    int b_len = D * matrix_len / 8;
-    std::cout << double(D * matrix_len) / 8.0 << "\n";
-    uint16_t H[matrix_len];
-    uint16_t K[matrix_len];
-    uint8_t b[b_len];
-    for (int i = 0; i < matrix_len; ++i)
-    {
-        H[i] = generateRandomNumber(15);
-        // H[i] = 0;
-        K[i] = 0;
-    }
-    for(int i = 0; i < b_len; ++i)
-    {
-        b[i] = 0;
-    }
+	int matrix_len = 8 * 8;
+	int b_len = matrix_len * B / 16;
+	uint16_t H[matrix_len];
+	uint16_t K[matrix_len];
+	uint16_t b[b_len], b_e[b_len];
+	for (int i = 0; i < matrix_len; ++i)
+	{
+		// H[i] = ;
+		H[i] = 0;
+		K[i] = 0;
+	}
+	for (int i = 0; i < b_len; ++i)
+	{
+		b[i] = generateRandomNumber(16);
+		b_e[i] = 0;
+	}
 
-    // std::string hex_H = "651610C02D3941111EEF61EF63DE5374";
-    // std::cout<<"H_hex: "<< frodo_hex(H,matrix_len)<<"\n";
-    // frodo_uint16(H,matrix_len,hex_H);
-    std::cout<<"H_test:"<<frodo_hex(H,matrix_len)<<"\n";
+// 	 // std::cout<<frodo_hex(b,b_len)<<"\n";
+// #ifdef FRODO
+// 	std::string hex_b = "C1E291EAB2B411FA9F5140FDDFB31F044E528B76216A6F2E";
+// #else
+// 	std::string hex_b = "C1E291EAB2B411FA9F5140FDDFB31F044E528B76216A6F2E";
+// #endif
+//
+	// frodo_uint16(b, b_len, hex_b);
+	std::cout << frodo_hex(b, b_len) << "\n";
+	frodo_encode(K, b);
+	std::cout << "K_hex :" << frodo_hex(K, matrix_len) << "\n";
+	frodo_decode(K, b_e);
 
-    frodo_pack(H,b,2,4);
-    std::cout<<"b_test:"<<frodo_hex(b,b_len)<<"\n";
-    frodo_unpack(K,b,2,4);
-    std::cout<<"K_test:"<<frodo_hex(K,matrix_len)<<"\n";
-   return 0;
+	std::cout <<"b_e_hex: "<< frodo_hex(b, b_len) << "\n";
+	std::cout << "b_e_hex: " << frodo_hex(b_e, b_len) << "\n";
+
+
+	// // std::string hex_H = "651610C02D3941111EEF61EF63DE5374";
+	// // std::cout<<"H_hex: "<< frodo_hex(H,matrix_len)<<"\n";
+	// // frodo_uint16(H,matrix_len,hex_H);
+	// std::cout<<"H_test:"<<frodo_hex(H,matrix_len)<<"\n";
+	//
+	// frodo_pack(H,b,2,4);
+	// std::cout<<"b_test:"<<frodo_hex(b,b_len)<<"\n";
+	// frodo_unpack(K,b,2,4);
+	// std::cout<<"K_test:"<<frodo_hex(K,matrix_len)<<"\n";
+	return 0;
 }
